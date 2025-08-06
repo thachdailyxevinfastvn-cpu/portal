34ddc2c3247a2ba05f5d590fc80189bbc5dd9592
Thach <thach@MacBook-Pro-cua-Thach.local>
2025-08-06 21:50:05 +0700

    feat: Dong bo menu va cap nhat mau sac cot noi that

--- config.js
+++ config.js
@@ -7,12 +7,6 @@
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw4zP-qroaAqCpLDS0nLKY4WxgaHZX2o6TURZ5FJh2IPRbVL8dQ3nsQ1MTukgIQjt7W/exec';
 
 // URL Apps Script cũ của Kho xe, có thể các trang khác vẫn cần dùng.
-const KHO_XE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx0xhtOAykaDNqt1ToOkrLEkxIaIRbFMWRO8qqv_eQ5Ws07L_7I11tTV0U2Gb7DXsVz9Q/exec';
-
-
-// --- LINK CHO PHƯƠNG PHÁP LẤY DỮ LIỆU CSV TRỰC TIẾP ---
-// URL này được dùng cho trang khoxe.html phiên bản mới để tránh lỗi.
-const KHO_XE_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTXy_q11K2YMC3SJ1jEMp_8GMTv5U1SUVRAiG7wDPkhXsBZ89qk3u-Vtw2vcgP-kjmFzEkX_rqGAs2E/pub?output=csv';
-
+const KHO_XE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwEQxdfuJHLMpU-gauE-scAar63uq6W-EJYMtIoJsR0HcTzrf9KY9JuSuinxU4hZIjKeg/exec';
 
 // Bạn có thể thêm các URL hoặc cấu hình khác ở đây trong tương lai.
