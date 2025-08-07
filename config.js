65b828d65e8a76c641440322684f3494d958bde6
Thach <thach@MacBook-Pro-cua-Thach.local>
2025-08-07 09:31:52 +0700

    feat: Dong bo menu va cap nhat mau sac cot noi that

--- config.js
+++ config.js
@@ -7,6 +7,6 @@
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw4zP-qroaAqCpLDS0nLKY4WxgaHZX2o6TURZ5FJh2IPRbVL8dQ3nsQ1MTukgIQjt7W/exec';
 
 // URL Apps Script cũ của Kho xe, có thể các trang khác vẫn cần dùng.
-const KHO_XE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwEQxdfuJHLMpU-gauE-scAar63uq6W-EJYMtIoJsR0HcTzrf9KY9JuSuinxU4hZIjKeg/exec';
+const KHO_XE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzxlKiAYsOF7_3EgWVp6nw7FIRMniFcaUIQEgS8fMeZrHWjjEsbjNVELavfn4SRJ_TJqg/exec';
 
 // Bạn có thể thêm các URL hoặc cấu hình khác ở đây trong tương lai.
