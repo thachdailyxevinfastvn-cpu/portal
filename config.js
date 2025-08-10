99e7e89bcea50eabd1691c489dc22506d5a2420d
Thach <thach@MacBook-Pro-cua-Thach.local>
2025-08-10 16:52:50 +0700

    feat: Dong bo menu va cap nhat mau sac cot noi that

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw4zP-qroaAqCpLDS0nLKY4WxgaHZX2o6TURZ5FJh2IPRbVL8dQ3nsQ1MTukgIQjt7W/exec';
 
 // URL Apps Script cho trang CHÍNH SÁCH
-const POLICY_PAGE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzroGyzJ_nJPF7BDB3JEc_lh6rLO8vIP07vPdZwQcTlOF5vaUnCh0u3fzreTKQ22SA_/exec';
+const POLICY_PAGE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzebZbQ2lve9aH9IvZsZjtA-g1TrosAEaoycQobhc0gBI-SpECsDqviLTk2OO_fnm4/exec';
 
 // URL Apps Script cũ của Kho xe, có thể các trang khác vẫn cần dùng.
 const KHO_XE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzxlKiAYsOF7_3EgWVp6nw7FIRMniFcaUIQEgS8fMeZrHWjjEsbjNVELavfn4SRJ_TJqg/exec';
