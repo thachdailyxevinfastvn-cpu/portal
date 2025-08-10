44d26bfb09848690d06a35a01524109fac7173b6
Thach <thach@MacBook-Pro-cua-Thach.local>
2025-08-10 11:37:03 +0700

    feat: Dong bo menu va cap nhat mau sac cot noi that

--- config.js
+++ config.js
@@ -3,12 +3,11 @@
 // =================================================================
 
 // --- LINK CHO APPS SCRIPT ---
+// URL của Google Apps Script để xử lý dữ liệu người dùng và các tác vụ khác.
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw4zP-qroaAqCpLDS0nLKY4WxgaHZX2o6TURZ5FJh2IPRbVL8dQ3nsQ1MTukgIQjt7W/exec';
+
+// URL Apps Script cũ của Kho xe, có thể các trang khác vẫn cần dùng.
 const KHO_XE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzxlKiAYsOF7_3EgWVp6nw7FIRMniFcaUIQEgS8fMeZrHWjjEsbjNVELavfn4SRJ_TJqg/exec';
 
-// === CẬP NHẬT CHO TRANG TẠO VĂN BẢN ===
-// URL cho file CHỈ XUẤT dữ liệu (sẽ lấy ở Bước 5)
-const TAO_VAN_BAN_GET_URL = 'https://script.google.com/macros/s/AKfycbyXXF92heosCXuTbnbxkQtI9UHr7pQxNTDOx70pIstxye2keAanT3eRr7gWj2FtlREh/exec';
-
-// URL cho file CHỈ NHẬP dữ liệu (sẽ lấy ở Bước 5)
-const TAO_VAN_BAN_POST_URL = 'https://script.google.com/macros/s/AKfycbyXXF92heosCXuTbnbxkQtI9UHr7pQxNTDOx70pIstxye2keAanT3eRr7gWj2FtlREh/exec';
+// URL Apps Script cho chức năng TẠO VĂN BẢN
+const TAO_VAN_BAN_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyXXF92heosCXuTbnbxkQtI9UHr7pQxNTDOx70pIstxye2keAanT3eRr7gWj2FtlREh/exec';
